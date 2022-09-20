d66c90bd1fbc3dcfe8698186c4131cc0855d638b
Arno Baert <arnobaert@jimbers-Air.localdomain>
2022-09-20 16:48:21 +0200

    new appicon

--- app/lib/helpers/env_config.dart
+++ app/lib/helpers/env_config.dart
@@ -3,6 +3,6 @@ import 'package:freeflow/helpers/environment.dart';
 abstract class EnvConfig {
   Environment environment = Environment.Staging;
 
-  String githash = "bb8c4f7";
-  String time = "16:21:18 20.09.2022";
+  String githash = "c84daea";
+  String time = "16:45:16 20.09.2022";
 }
